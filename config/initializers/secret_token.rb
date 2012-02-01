e012fcb80ac4a850616693b5744ab8f4ae2295c8
Magesh <pingmagesh@gmail.com>
2012-02-01 23:53:11 +0530

    my first commit for the mydiary opensource project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mydiary::Application.config.secret_token = '6056e1ee2c5de7c3b348293b8df53b60c3a2db679a5e828a9621526dd485ce63dc91bb6e68a8f1fc952a742b40ad0bd688dff165fdf25e6a0226dbd6fdfd9424'
