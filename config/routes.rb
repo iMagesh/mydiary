63d9f12aeab190851660ed3be04636e549cb8228
Magesh <pingmagesh@gmail.com>
2012-06-24 16:35:07 +0530

    added user and session, created a simple authentication system via railscast

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Mydiary::Application.routes.draw do
-  resources :posts
 
+  namespace :admin do
+    resources :posts
+    resources :users
+  end
+
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  root :to => "users#new"
+  resources :sessions
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
